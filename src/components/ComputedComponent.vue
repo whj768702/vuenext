b8753de6a3015b354c90804efa06938f3707a9a9
whj768702 <whj768702@gmail.com>
2020-07-08 18:00:34 +0800

    feat(computed): computed with get and set api demo.

--- src/components/ComputedComponent.vue
+++ src/components/ComputedComponent.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="computed-wrap">
-    <h3>computed api demo</h3>
-    <p>primary value: {{counter}} </p>
-    <p>computed value(add 2): {{computedCounter}}</p>
-    <button @click="addOne">add 1</button>
+    <div class="basic">
+      <h3>computed api demo</h3>
+      <p>primary value: {{counter}} </p>
+      <p>computed value(add 2): {{computedCounter}}</p>
+      <button @click="addOne">add 1</button>
+    </div>
+    <div class="with-set-get">
+      <h3>computed with set and get</h3>
+      <p>primary value: {{counter2}}</p>
+      <p>computed value(add 2): {{computedCounter2}}</p>
+      <button @click="addOne2">add 1</button>
+      <button @click="addComputedOne">computed value add 1</button>
+    </div>
   </div>
 </template>
 
@@ -15,14 +24,30 @@
     setup() {
       const counter = ref(0);
       const computedCounter = ref(computed(() => counter.value + 2));
+
+      const counter2 = ref(0);
+      const computedCounter2 = ref(computed({
+        get: () => counter2.value,
+        set: value => {
+          counter2.value = value;
+        }
+      }));
       return {
         counter,
-        computedCounter
+        counter2,
+        computedCounter,
+        computedCounter2
       };
     },
     methods: {
       addOne() {
         this.counter++;
+      },
+      addOne2() {
+        this.counter2++;
+      },
+      addComputedOne() {
+        this.computedCounter2++;
       }
     }
   };
