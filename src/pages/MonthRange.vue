bc94de7baf05ba193ff01524d7edc369a0211441
whj768702 <whj768702@gmail.com>
2022-03-10 14:37:58 +0800

    refactor(组件): 重构月范围选择逻辑.

--- src/pages/MonthRange.vue
+++ src/pages/MonthRange.vue
@@ -6,9 +6,9 @@
       <button @click="nextYear">&gt;</button>
     </div>
     <div class="flex flex-wrap gap-2 w-[220px] border py-4">
-      <div v-for="month in [1,2,3,4,5,6,7,8,9,10,11,12]" class="w-[30%] text-center"
-           :class="[isSelected(year,month-1), isIncluded(year,month-1)]"
-           :key="month" @click="chooseMonth(year, month-1)">{{ month }}月
+      <div v-for="month in months" class="w-[30%] text-center"
+           :class="[isSelected(month), isIncluded(month)]"
+           :key="month" @click="chooseMonth(month)">{{ month.format('MM') }}月
       </div>
     </div>
   </div>
@@ -29,36 +29,44 @@ export default {
       selectedMonth: [],
     };
   },
+  computed: {
+    months() {
+      let result = [];
+      for (let i = 0; i < 12; i++) {
+        result.push(this.year.add(i, 'month'));
+      }
+      return result;
+    },
+  },
   methods: {
     previousYear() {
-      this.selectedMonth = null;
+      this.selectedMonth = [];
       this.year = this.year.subtract(1, 'year');
     },
     nextYear() {
-      this.selectedMonth = null;
+      this.selectedMonth = [];
       this.year = this.year.add(1, 'year');
     },
-    chooseMonth(year, month) {
+    chooseMonth(month) {
       if (!this.selectedMonth.length) {
-        this.selectedMonth = [this.year.add(month, 'month')];
+        this.selectedMonth = [month];
       } else if (this.selectedMonth.length === 1) {
-        if (this.year.add(month, 'month').isAfter(this.selectedMonth[0])) {
-          this.selectedMonth = [this.selectedMonth[0], this.year.add(month, 'month')];
+        if (month.isAfter(this.selectedMonth[0])) {
+          this.selectedMonth = [this.selectedMonth[0], month];
         } else {
-          this.selectedMonth = [this.year.add(month, 'month'), this.selectedMonth[0]];
+          this.selectedMonth = [month, this.selectedMonth[0]];
         }
       }
-      console.log('selectedMonth', month, this.year.format('YYYY-MM'), this.selectedMonth.map(item => item.format('YYYY-MM')));
+      console.log('selectedMonth', this.selectedMonth.map(item => item.format('YYYY-MM')));
     },
-    isSelected(year, month) {
+    isSelected(month) {
       let result = false;
-      result = this.selectedMonth[0]?.isSame(year.add(month, 'month')) || this.selectedMonth[1]?.isSame(year.add(month, 'month'));
+      result = this.selectedMonth[0]?.isSame(month) || this.selectedMonth[1]?.isSame(month);
       return result ? 'bg-[tomato]' : '';
     },
-    isIncluded(year, month) {
+    isIncluded(month) {
       if (this.selectedMonth.length === 2) {
-        let result = this.year.add(month, 'month').isBetween(this.selectedMonth[0], this.selectedMonth[1], 'month');
-        console.log(dayjs(`${year}-${month}`).format('yyyy-MM'), result);
+        let result = month.isBetween(this.selectedMonth[0], this.selectedMonth[1], 'month');
         return result ? 'bg-[tomato] bg-opacity-10' : '';
       }
     },
